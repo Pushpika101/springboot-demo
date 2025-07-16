e6ac85896a89569525f795653d9b9a9b8674ca59
Pushpika <e21442@eng.pdn.ac.lk>
2025-07-17 02:09:54 +0530

    add autowire

--- src/main/java/com/example/demo/student/studentcontoller.java
+++ src/main/java/com/example/demo/student/studentcontoller.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import java.util.List;
 public class studentcontoller {
 
     private final studentservice studentservice;
-
+    @Autowired
     public studentcontoller(studentservice studentservice) {
         this.studentservice = studentservice;
     }
--- src/main/java/com/example/demo/student/studentservice.java
+++ src/main/java/com/example/demo/student/studentservice.java
@@ -1,9 +1,12 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class studentservice {
     public List<student> getstudents() {
         return List.of(
